8d7b79cd082a6a7d183435a952de8c330cd18466
ming-napat <nchimsumran1@nait.ca>
2024-01-10 16:49:24 -0700

    multiple profiles

---
+++ ProfileExample/Assets/Scripts/ProfileButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileButton : MonoBehaviour
+{
+    [SerializeField] int profileNo;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- ProfileExample/Assets/Scripts/SaveLoadTest.cs
+++ ProfileExample/Assets/Scripts/SaveLoadTest.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Transactions;
 using UnityEngine;
 using System.IO;
-using System.Xml;
 using System.Xml.Serialization;
 using TMPro;
 
@@ -12,12 +8,26 @@ public class SaveLoadTest : MonoBehaviour
 {
     [SerializeField] NameData myName;
     [SerializeField] TMP_InputField myInputField;
-    // Start is called before the first frame update
+
+    [SerializeField] GameObject addProfilePanel;
+    [SerializeField] GameObject mainProfileMenuPanel;
+
+    //[SerializeField] TextMeshProUGUI error;
+
+    [SerializeField] GameObject[] profileButtons;
+
+    [SerializeField] int currentProfile;
+
+    private string path = $"SaveData\\Profile1";
+
     void Start()
     {
+        currentProfile = 0;
         myName = new NameData();
 
-        LoadData();
+        //addProfilePanel.SetActive(false);
+        //mainProfileMenuPanel.SetActive(true);
+        //LoadData();
     }
 
     public void ChangeName(string newName)
@@ -25,23 +35,79 @@ public class SaveLoadTest : MonoBehaviour
         myName.playerName = newName;
     }
 
+    //private bool CheckDuplicateName(string name)
+    //{
+    //    return myNameList.Any(x => x.playerName == name);
+    //}
+
+    public void ChangeProfile(int profileNo)
+    {
+        path = $"SaveData\\Profile{currentProfile + 1}";
+        currentProfile = profileNo;
+    }
+
     public void SaveData()
     {
-        Stream stream = File.Open("PlayerData", FileMode.Create);
+        //error.text = "";
+        CreateFile();
+        Stream stream = File.Open(path + "\\PlayerData", FileMode.Create);
         XmlSerializer serializer = new XmlSerializer(typeof(NameData));
         serializer.Serialize(stream, myName);
         stream.Close();
+        //if (!CheckDuplicateName(myName.playerName))
+        //{
+        //    myNameList[myName.no] = myName;
+        //    Stream stream = File.Open(path, FileMode.Create);
+        //    XmlSerializer serializer = new XmlSerializer(typeof(NameData));
+        //    serializer.Serialize(stream, myNameList);
+        //    stream.Close();
+        //}
+        //else
+        //{
+        //    error.text = $"{myName.playerName} is already exists.";
+        //}
     }
 
     public void LoadData()
     {
-        Stream stream = File.Open("PlayerData", FileMode.Open);
-        XmlSerializer serializer = new XmlSerializer(typeof(NameData));
-        myName = (NameData)serializer.Deserialize(stream);
-        stream.Close();
-
-        myInputField.text = myName.playerName;
+        if (File.Exists(path))
+        {
+            Stream stream = File.Open(path + "\\PlayerData", FileMode.Open);
+            XmlSerializer serializer = new XmlSerializer(typeof(NameData));
+            myName = (NameData)serializer.Deserialize(stream);
+            //string json = File.ReadAllText(path);
+            //myNameList = JsonConvert.DeserializeObject<List<NameData>>(json);
+            //(NameData)serializer.Deserialize<NameData>(stream);
+            stream.Close();
+            myInputField.text = myName.playerName;
+        }
     }
+
+    void CreateFile()
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    //public void SaveData()
+    //{
+    //    Stream stream = File.Open("PlayerData", FileMode.Create);
+    //    XmlSerializer serializer = new XmlSerializer(typeof(NameData));
+    //    serializer.Serialize(stream, myName);
+    //    stream.Close();
+    //}
+
+    //public void LoadData()
+    //{
+    //    Stream stream = File.Open("PlayerData", FileMode.Open);
+    //    XmlSerializer serializer = new XmlSerializer(typeof(NameData));
+    //    myName = (NameData)serializer.Deserialize(stream);
+    //    stream.Close();
+
+    //    myInputField.text = myName.playerName;
+    //}
 
 }
 
