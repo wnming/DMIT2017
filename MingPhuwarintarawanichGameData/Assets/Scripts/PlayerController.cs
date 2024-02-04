361360c6bed2dcf7b821a12502e7103fedf482ea
ming-napat <nchimsumran1@nait.ca>
2024-02-04 01:58:43 -0700

    Done

--- MingPhuwarintarawanichGameData/Assets/Scripts/PlayerController.cs
+++ MingPhuwarintarawanichGameData/Assets/Scripts/PlayerController.cs
@@ -13,13 +13,26 @@ public class PlayerController : MonoBehaviour
     Image fogPanel;
     [SerializeField]
     bool fade, fadeOn;
-    
+    private float direction;
+    private float sensitivity = 20.0f;
+
+    private bool isPartical;
+    [SerializeField]
+    GameObject transitionParticle;
+
+    float initialCamera;
+
     // Start is called before the first frame update
     void Start()
     {
+        initialCamera = Camera.main.fieldOfView;
         anim = GetComponentInChildren<Animator>();
         movementSpeed = 1.0f;
         rotationSpeed = 100.0f;
+
+        direction = 100.0f;
+
+        isPartical = false;
     }
 
     // Update is called once per frame
@@ -28,26 +41,65 @@ public class PlayerController : MonoBehaviour
         if (!fade)
         {
             vInput = Input.GetAxis("Vertical");
-            hInput = Input.GetAxis("Horizontal");               
+            hInput = Input.GetAxis("Horizontal");
+
+            //Camera.main.fieldOfView = initialCamera;
         }
         else
         {
             vInput = 0;
             hInput = 0;
-            if (fadeOn && fogPanel.color.a < 1)
+            if (fadeOn && direction > -100)
             {
-                //Debug.Log(fogPanel.color.a);
-                fogPanel.color = new Color(fogPanel.color.r, fogPanel.color.g, fogPanel.color.b, fogPanel.color.a + Time.deltaTime);
-            }
-            else if (!fadeOn && fogPanel.color.a > 0)
-            {
-                fogPanel.color = new Color(fogPanel.color.r, fogPanel.color.g, fogPanel.color.b, fogPanel.color.a - Time.deltaTime);
+                if (!isPartical)
+                {
+                    isPartical = true;
+                    GameObject particle = Instantiate(transitionParticle, transform.position, transform.rotation);
+                    Destroy(particle, 1.8f);
+                }
+                float cam = Camera.main.fieldOfView;
+                cam += -1 * sensitivity * Time.deltaTime;
+                Camera.main.fieldOfView = cam;
+                direction -= 1;
             }
             else
             {
-                fade = false;
+                if (!fadeOn && direction < 100)
+                {
+                    if (!isPartical)
+                    {
+                        isPartical = true;
+                        GameObject particle = Instantiate(transitionParticle, transform.position, transform.rotation);
+                        Destroy(particle, 1.8f);
+                    }
+                    float cam = Camera.main.fieldOfView;
+                    cam += 1 * sensitivity * Time.deltaTime;
+                    Camera.main.fieldOfView = cam;
+                    direction += 1;
+                }
+                else
+                {
+                    if(Camera.main.fieldOfView > initialCamera)
+                    {
+                        Camera.main.fieldOfView = initialCamera;
+                    }
+                    isPartical = false;
+                    fade = false;
+                }
             }
-            
+            //if (fadeOn && fogPanel.color.a < 1)
+            //{
+            //    //Debug.Log(fogPanel.color.a);
+            //    fogPanel.color = new Color(fogPanel.color.r, fogPanel.color.g, fogPanel.color.b, fogPanel.color.a + Time.deltaTime);
+            //}
+            //else if (!fadeOn && fogPanel.color.a > 0)
+            //{
+            //    fogPanel.color = new Color(fogPanel.color.r, fogPanel.color.g, fogPanel.color.b, fogPanel.color.a - Time.deltaTime);
+            //}
+            //else
+            //{
+            //    fade = false;
+            //}
         }
 
         transform.position = new Vector3(transform.position.x, 0, transform.position.z);
